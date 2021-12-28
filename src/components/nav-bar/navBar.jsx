90b9f18af20b1f00b77db7d00ab23b8a1d0fea19
Marla Schulz <marlaa.schulz@gmail.com>
2021-12-28 15:01:34 -0600

    Add basic styling

--- src/components/nav-bar/navBar.jsx
+++ src/components/nav-bar/navBar.jsx
@@ -1,7 +1,16 @@
-function navBar() {
+import './navBar.css'
+import {NavLink} from 'react-router-dom'
+
+function navBar({ title }) {
   return (
-    <div>
-      
+    <div className="nav-bar">
+      <h1 className="site-title">{title}</h1>
+      <div className="menu">
+        <NavLink exact activeClassName='active' to='/'>Home</NavLink>
+        <NavLink activeClassName='active' to='/about'>About</NavLink>
+        <NavLink activeClassName='active' to='/media'>Media</NavLink>
+        <NavLink activeClassName='active' to='/contact'>Contact</NavLink>
+      </div>
     </div>
   )
 }
